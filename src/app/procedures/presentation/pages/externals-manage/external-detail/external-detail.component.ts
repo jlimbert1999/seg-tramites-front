4250a0f27a709b6410cc5ab7216acee16b92902b
josel <jose@gmail.com>
2024-10-18 18:14:42 -0400

    componente detalle externo

--- src/app/procedures/presentation/pages/externals-manage/external-detail/external-detail.component.ts
+++ src/app/procedures/presentation/pages/externals-manage/external-detail/external-detail.component.ts
@@ -1,12 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-external-detail',
   standalone: true,
-  imports: [CommonModule, MatTabsModule],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './external-detail.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ExternalDetailComponent {}
+export default class ExternalDetailComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+
+
+
+  ngOnInit(): void {
+    // this.heroes$ = this.route.paramMap.pipe(
+    //   switchMap((params) => {
+    //     this.selectedId = Number(params.get('id'));
+    //     return this.service.getHeroes();
+    //   })
+    // );
+  }
+}
